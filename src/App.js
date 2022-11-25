fbb637b3b24d5b3d179b472cfbc1f9a2a561e752
Marcelo1080p <juniooor.2015@gmail.com>
2022-11-24 22:36:26 -0300

    criando visual dos personagens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react';
+import { personagem } from './api/api';
 import './App.css';
 
 function App() {
+
+  const[showCharacter, setShowCharacter] = useState([])
+  
+  useEffect(() => {
+    personagem().then(res => setShowCharacter(res))
+    
+  },[])
+
   return (
     <div className="App">
-    
+      {showCharacter.map((person) => (
+        <div className='container'>
+          <p>{person.name}</p>
+          <p>{person.status}</p>
+          <p>{person.species}</p>
+          <p>{person.type}</p>
+        </div>
+      ))}
     </div>
   );
 }
