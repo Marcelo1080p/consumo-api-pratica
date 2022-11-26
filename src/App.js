591f37ca7a3ab4a4eccf0f258671761e6fb22111
Marcelo1080p <juniooor.2015@gmail.com>
2022-11-25 22:42:53 -0300

    realizado atualizações dos cards

--- src/App.js
+++ src/App.js
@@ -14,16 +14,21 @@ function App() {
   return (
     <div className="App">
       <div className='containerPersonagens'>
-        {showCharacter.map((person) => (
-          <div className='container'>
-            <img src={person.image} alt={`Personagem ${person.name}`}/>
-            <p>{person.name}</p>
-            <p>{person.status}</p>
-            <p>{person.species}</p>
-            <p>{person.type}</p>
-          </div>
-        ))}
-
+          {showCharacter.map((person) => (
+            <div className='box'>
+              <span></span>
+              <section>
+              <img src={person.image} alt={`Personagem ${person.name}`}/>
+              <div className='details'>
+              <p>{person.name}</p>
+              <p>{person.status}</p>
+              <p>{person.species}</p>
+              <p>{person.type}</p>
+              </div>
+              </section>
+            </div>
+          ))}
+        
       </div>
     </div>
   );
